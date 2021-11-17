9a45f4fe5dd8cd51f10d6424128a72ba23f73370
Bruna Silva <beyss21@gmail.com>
2021-11-17 18:55:34 -0300

    Tornando super class abstrata

--- projetoJavaB1/src/projetoJavaB1/Energia.java
+++ projetoJavaB1/src/projetoJavaB1/Energia.java
@@ -1,14 +1,13 @@
 package projetoJavaB1;
 
-public class Energia {
+import java.util.Scanner;
+
+public abstract class Energia {
 
 	private String nomeEmpresa;
 	private String cnpj;
-	private String fonte;
 	private float consumo;
-	private int vidaUtil;
 	private double orcamento;
-	private float capacidade;
 	private String regiao;
 	private float faturamento;
 	private double percAbt;
@@ -25,13 +24,33 @@ public class Energia {
 		this.percAbt = percAbt;
 
 	}
+	
+	Scanner leia = new  Scanner(System.in);
 
-	public Energia(String fonte, int vidaUtil, float capacidade) {
-		this.fonte = fonte;
-		this.vidaUtil = vidaUtil;
-		this.capacidade = capacidade;
-
-	}
+    public void cadastroEmpresa()
+    {
+        System.out.println("\nNome da Empresa: ");
+        this.nomeEmpresa = leia.next();
+        System.out.println("\nCNPJ: ");
+        this.cnpj = leia.next();
+        System.out.println("\nMédia de consumo de energia da empresa: ");
+        this.consumo = leia.nextFloat();
+        System.out.println("\nOrçamento disponível: ");
+        this.orcamento = leia.nextDouble();
+        System.out.println("\nRegião que a empresa se situa: ");
+        System.out.println("\n1- Norte ");
+        System.out.println("\n2- Nordeste ");
+        System.out.println("\n3- Centro oeste ");
+        System.out.println("\n4- Sudeste ");
+        System.out.println("\n5- Sul ");
+        this.regiao = leia.next();
+        System.out.println("\nFaturamento da empresa: ");
+        this.faturamento = leia.nextFloat();
+    }
+	
+	abstract public void fonte (String fonte);
+	abstract public void vidaUtil (int V);
+	abstract public void capacidade (float capacidade);
 
 	public String getNomeEmpresa() {
 		return nomeEmpresa;
@@ -49,14 +68,6 @@ public class Energia {
 		this.cnpj = cnpj;
 	}
 
-	public String getFonte() {
-		return fonte;
-	}
-
-	public void setFonte(String fonte) {
-		this.fonte = fonte;
-	}
-
 	public float getConsumo() {
 		return consumo;
 	}
@@ -65,28 +76,12 @@ public class Energia {
 		this.consumo = consumo;
 	}
 
-	public int getVidaUtil() {
-		return vidaUtil;
-	}
-
-	public void setVidaUtil(int vidaUtil) {
-		this.vidaUtil = vidaUtil;
-	}
-
 	public double getOrcamento() {
 		return orcamento;
 	}
 
 	public void setOrcamento(double orcamento) {
 		this.orcamento = orcamento;
-	}
-
-	public float getCapacidade() {
-		return capacidade;
-	}
-
-	public void setCapacidade(float capacidade) {
-		this.capacidade = capacidade;
 	}
 
 	public String getRegiao() {
--- projetoJavaB1/src/projetoJavaB1/EnergiaSolar.java
+++ projetoJavaB1/src/projetoJavaB1/EnergiaSolar.java
@@ -2,12 +2,37 @@ package projetoJavaB1;
 
 public class EnergiaSolar extends Energia {
 		
+		public EnergiaSolar (String tipo,String nomeEmpresa, String cnpj, float consumo, double orcamento, String regiao, float faturamento,double percAbt)
+		{
+			super(nomeEmpresa,cnpj,consumo,orcamento,regiao,faturamento,percAbt);
+			this.tipo = tipo;
+		}
+		
 		private String tipo;
 		
-		public EnergiaSolar(String fonte, int vidaUtil, float capacidade, String tipo)
+		public void imprimirInfo() 
 		{
-			super(fonte,vidaUtil,capacidade);
-			this.tipo = tipo;
+			System.out.println("\nNome da Empresa: " + getNomeEmpresa() + "CPF: " + getCnpj() + "\nConsumo: " + getConsumo()
+					+ "Orçamento: " + getOrcamento() + "\nRegiao: " + getRegiao() + "Faturamento: " + getFaturamento()
+					+ "Percentual: " + getPercAbt());
+		}
+		
+		@Override
+		public void fonte (String fonte)
+		{
+			System.out.println("A fonta da energia é o sol.");
+		}
+		
+		@Override
+		public void vidaUtil (int V)
+		{
+			System.out.println("A vida últi é "+25+" anos, funcionando com 80% do seu desempenho original.");
+		}
+		
+		@Override 
+		public void capacidade (float capacidade)
+		{
+			System.out.println("No Brasil, a capacidade é de "+10+"GW de potência");
 		}
 
 		public String getTipo() {
