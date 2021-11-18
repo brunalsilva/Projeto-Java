95adc5e8b4a6d39a2d4bbaa65e54bbb4d0f4fb1b
GuilhermeRivas <guilhermerivascontato@gmail.com>
2021-11-18 18:45:46 -0300

    Alteração dos arrays para Subclasse Cadastro, e otimização do código de cadastramento

--- projetoJavaB1/src/projetoJavaB1/Empresa.java
+++ projetoJavaB1/src/projetoJavaB1/Empresa.java
@@ -1,5 +1,6 @@
 package projetoJavaB1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Empresa extends Energia {
@@ -10,14 +11,34 @@ public class Empresa extends Energia {
 		// TODO Auto-generated constructor stub
 		
 	}
+	
+		ArrayList<String> empresas = new ArrayList();
+		ArrayList<String> cnpjs = new ArrayList();
+		ArrayList<Integer> regiaoN = new ArrayList();
+		ArrayList<String> regioesTxt = new ArrayList();
+	
 		public int op = 0,lugar;
+		public int numE = 0,n;
 		
 		Scanner leia = new Scanner(System.in);
 		@Override
-		  public void cadastroEmpresa()
+		  public  void cadastroEmpresa()
+
+		    {do 
 		    {
+		    	System.out.println("----------------------------------------");
+		    	System.out.println("\n(1) - Cadastrar nova empresa: ");
+		    	System.out.println("\n(2) - Me mostre a lista de empresas cadastradas, seus CNPJS e suas regiões: ");
+		    	System.out.println("\n(3) - Cancelar operação. ");
+		    	System.out.println("\n\t Escolha uma opção. ");
+		    	System.out.println("----------------------------------------");
+		    	op = leia.nextInt();
+		    	switch(op) 
+		    	{
+		    	case 1:
 		        System.out.println("\nNome da Empresa: ");
 		        this.nomeEmpresa = leia.next();
+		        empresas.add(getNomeEmpresa());
 		        System.out.println("\nCNPJ: ");
 		        this.cnpj = leia.next();
 		        do
@@ -30,6 +51,7 @@ public class Empresa extends Energia {
 		        	}
 		       	       
 		        }while(getCnpj().length() != 14);
+		        cnpjs.add(getCnpj());
 		        System.out.println("\nConsumo médio de energia da empresa em KWh/h: ");
 		        this.consumo = leia.nextFloat();
 		        System.out.println("\nOrçamento disponível: ");
@@ -42,12 +64,16 @@ public class Empresa extends Energia {
 		        System.out.println("\n5- Sul ");
 		        this.regiao= leia.nextInt();
 		        if(getRegiao() >= 1 && getRegiao() <= 5)
+		        	
 		        {
+		        	regiaoN.add(getRegiao());
 		        	System.out.println("\nFaturamento da empresa: ");
 			        this.faturamento = leia.nextFloat();
-		        }else {
-		        	
-		       do {
+		        }
+		        else 
+		        {		        	
+		       do 
+		       {
 		        System.out.println("\nRegião que a empresa se situa: ");
 		        System.out.println("\n1- Norte ");
 		        System.out.println("\n2- Nordeste ");
@@ -55,16 +81,52 @@ public class Empresa extends Energia {
 		        System.out.println("\n4- Sudeste ");
 		        System.out.println("\n5- Sul ");
 		        this.regiao= leia.nextInt();
-		        } while(getRegiao() < 1 || getRegiao() > 5);
-		        
-		        	
-		        
-		        
+		        }while(getRegiao() < 1 || getRegiao() > 5);  
+		       	regiaoN.add(getRegiao());
 		        System.out.println("\nFaturamento da empresa: ");
 		        this.faturamento = leia.nextFloat();
-		        }
+		       				
+		        }break;
+		    	case 2:
+		    	{
+		    		for(int i = 0;i<regiaoN.size();i++)
+		    		{
+		    			if(regiaoN.get(i)  == 1)
+		    			{
+		    				regioesTxt.add("Norte");
+		    			}
+		    			if(regiaoN.get(i)  == 2)
+		    			{
+		    				regioesTxt.add("Nordeste");
+		    			}
+		    			if(regiaoN.get(i)  == 3)
+		    			{
+		    				regioesTxt.add("Centro oeste");
+		    			}
+		    			if(regiaoN.get(i)  == 4)
+		    			{
+		    				regioesTxt.add("Sudeste");
+		    			}
+		    			if(regiaoN.get(i) == 5)
+		    			{
+		    				regioesTxt.add("Sul");
+		    			}
+		    			
+		    		}
+		    		System.out.println("As empresas cadastradas são: "+empresas);	
+					System.out.println("Os CNPJs das empresas cadastradas são: "+cnpjs);
+					System.out.println("A região que as empresas se situam: "+regioesTxt);
+		    	}break;
+		    	default:
+		    		System.out.println("\nOperação Cancelada.");
+		    		System.out.println("\n\n\n\n\n\n\n\n");
+		    		
+		    	}
+		    }while(op != 3);
 		    }
-
+		
+		
+		
 		@Override
 		public void fonte(String fonte) {
 			
