4d208d3deda50af1ed13cab1dc577661369c4478
GuilhermeRivas <guilhermerivascontato@gmail.com>
2021-11-18 18:48:57 -0300

    teste cadastro bonito e esbelto

--- projetoJavaB1/src/projetoJavaB1/TesteCadastro.java
+++ projetoJavaB1/src/projetoJavaB1/TesteCadastro.java
@@ -7,12 +7,6 @@ public class TesteCadastro {
 	public static void main(String[] args) {
 			  	  
 		Empresa emp1 = new Empresa(null, null, 0, 0, 0, 0, 0);
-<<<<<<< HEAD
-	
-=======
-		Empresa emp2 = new Empresa(null, null, 0, 0, 0, 0, 0);
-		Empresa emp3 = new Empresa(null, null, 0, 0, 0, 0, 0);
-		Empresa emp4 = new Empresa(null, null, 0, 0, 0, 0, 0);
 		
 		System.out.println("============================================================================================================");
 		System.out.println("_____________         _____                        ________                ________       _____        \r\n"
@@ -23,48 +17,12 @@ public class TesteCadastro {
 				+ "                                                                           /___/");
 		System.out.println("============================================================================================================");
 		System.out.println("Prezade Cliente, seja Bem-Vinde ao Futuro!!\nPor favor, prossiga com o seu Cadastro.");
-		ArrayList<String> empresas = new ArrayList();
-		ArrayList<String> cnpjs = new ArrayList();
-		ArrayList<Integer> regiao = new ArrayList();
-		ArrayList<String> regioesTxt = new ArrayList();
->>>>>>> 12828140bd3cd1194376efc7c1dfa042fb03b0fd
+	
+
 		
 		emp1.cadastroEmpresa();
+			
 		
-		
-		
-<<<<<<< HEAD
-=======
-		for(int i = 0;i<regiao.size();i++)
-		{
-			if(regiao.get(i)  == 1)
-			{
-				regioesTxt.add("Norte");
-			}
-			if(regiao.get(i)  == 2)
-			{
-				regioesTxt.add("Nordeste");
-			}
-			if(regiao.get(i)  == 3)
-			{
-				regioesTxt.add("Centro oeste");
-			}
-			if(regiao.get(i)  == 4)
-			{
-				regioesTxt.add("Sudeste");
-			}
-			if(regiao.get(i) == 5)
-			{
-				regioesTxt.add("Sul");
-			}
-		}
-		
-		System.out.println("As empresas cadastradas são: "+empresas);	
-		System.out.println("Os CNPJs das empresas cadastradas são: "+cnpjs);
-		System.out.println("A região que as empresas se situam: "+regioesTxt);
-		
-	}
->>>>>>> 12828140bd3cd1194376efc7c1dfa042fb03b0fd
 }
 	}
 
